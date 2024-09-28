1ef5b7b2c9a6b0c8a13059c43c8c226b11326208
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2024-09-27 21:21:43 -0600

    added final animations

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,25 @@ import Quadrant from "./components/Quadrant/Quadrant";
 // DEFINING FOOD QUADRANT ASSETS
 import idleFoodCharacterPath from "./assets/food/idleCharacter.png";
 import unselectedFoodQuadrantPath from "./assets/food/unselected.png";
-// This can be set like the others as a file path once the real gif is added to the assets directory
-const temporaryFoodGif =
-  "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXFtOGlwYXVodXp1NTlhMzNneWlsOTk3eXRydW1vdjNjMWhmZXd4eSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/K4x1ZL36xWCf6/200.webp";
+import foodAnimationPath from "./assets/food/animation.gif";
 
 // DEFINING MUSIC QUADRANT ASSETS
 import idleMusicCharacterPath from "./assets/music/idleCharacter.png";
 import unselectedMusicQuadrantPath from "./assets/music/unselected.png";
-// This can be set like the others as a file path once the real gif is added to the assets directory
-const temporaryMusicGif = 
-  'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXZ3MTQ4Y2h3MmtqcGk4ZjF6a2R6aDd1YmZnczd3dGc4a2p2a3I4ZCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/tJU72w9lPzUPe/200.webp';
+import musicAnimationPath from "./assets/music/animation.gif";
 
 // DEFINING PROGRAMMING QUADRANT ASSETS
-import idleSleepCharacterPath from "./assets/programming/idleCharacter.png";
-import unselectedSleepQuadrantPath from "./assets/programming/unselected.png";
-// This can be set like the others as a file path once the real gif is added to the assets directory
-const temporarySleepGif =
-  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2dmc2t3N29pbTd5MjNsNjg2YW5wM2xxdnoxMnQ1aXN3ZXhoN2NoaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/QjdvZSi8f5JsXouSFp/200.webp";
+import idleProgrammingCharacterPath from "./assets/programming/idleCharacter.png";
+import unselectedProgrammingQuadrantPath from "./assets/programming/unselected.png";
+import programmingAnimationPath from "./assets/programming/animation.gif";
 
 // DEFINING WORKOUT QUADRANT ASSETS
 import idleWorkoutCharacterPath from "./assets/workout/idleCharacter.png";
 import unselectedWorkoutQuadrantPath from "./assets/workout/unselected.png";
+import workoutAnimationPath from "./assets/workout/animation.gif";
+
 import DraggableButton from "./components/DraggableButton/DraggableButton";
 import StalkerButton from "./components/StalkerButton.tsx/StalkerButton";
-// This can be set like the others as a file path once the real gif is added to the assets directory
-const temporaryWorkoutGif =
-  "https://media3.giphy.com/media/W7dBXzbnEpOBG/giphy.webp?cid=790b7611eoq6ivp1ebw9gc6oyq1qq5daq6u3z99eefcf2xkl&ep=v1_gifs_search&rid=giphy.webp&ct=g";
 
 function App() {
   const { setCharacterIdle, characterIdle } = useData();
@@ -50,7 +43,7 @@ function App() {
               index={1}
               unselectedQuadrantAssetPath={unselectedFoodQuadrantPath}
               idleCharacterAssetPath={idleFoodCharacterPath}
-              animatedCharacterAssetPath={temporaryFoodGif}
+              animatedCharacterAssetPath={foodAnimationPath}
             ></Quadrant>
           </div>
           <div className="quadrant-container">
@@ -58,7 +51,7 @@ function App() {
               index={2}
               unselectedQuadrantAssetPath={unselectedMusicQuadrantPath}
               idleCharacterAssetPath={idleMusicCharacterPath}
-              animatedCharacterAssetPath={temporaryMusicGif}
+              animatedCharacterAssetPath={musicAnimationPath}
             ></Quadrant>
           </div>
         </div>
@@ -66,9 +59,9 @@ function App() {
           <div className="quadrant-container">
             <Quadrant
               index={3}
-              unselectedQuadrantAssetPath={unselectedSleepQuadrantPath}
-              idleCharacterAssetPath={idleSleepCharacterPath}
-              animatedCharacterAssetPath={temporarySleepGif}
+              unselectedQuadrantAssetPath={unselectedProgrammingQuadrantPath}
+              idleCharacterAssetPath={idleProgrammingCharacterPath}
+              animatedCharacterAssetPath={programmingAnimationPath}
             ></Quadrant>
           </div>
           <div className="quadrant-container">
@@ -76,7 +69,7 @@ function App() {
               index={4}
               unselectedQuadrantAssetPath={unselectedWorkoutQuadrantPath}
               idleCharacterAssetPath={idleWorkoutCharacterPath}
-              animatedCharacterAssetPath={temporaryWorkoutGif}
+              animatedCharacterAssetPath={workoutAnimationPath}
             ></Quadrant>
           </div>
         </div>
